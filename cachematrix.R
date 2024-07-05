83c311a4679d3a8782becc61158578a8ec09dc44
T P <tp@Ts-MacBook-Air.local>
2024-07-05 11:08:05 -0500

    R Program

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        I <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x 
+        setI <- function(inverse) I <<- inverse
+        getI <- function() m 
+        list(set=set, get=get, setI=setI, getI=getI)            
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getI()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$setmean(I)
+        I
 }
+
